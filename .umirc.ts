4c9c96bd4b1a068636e85b333f399a40285182c0
Floris Van den Abeele <floris.vandenabeele@external.atlascopco.com>
2021-12-24 13:24:14 +0100

    use webpack plugin to dump config

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'umi';
 
+import { WebpackConfigDumpPlugin } from 'webpack-config-dump-plugin';
+
 export default defineConfig({
+  chainWebpack: function (config, { webpack }) {
+    // Dump webpack config:
+    config
+      .plugin('webpack-config-dump-plugin')
+      .use(WebpackConfigDumpPlugin, [{ name: 'webpack-dump.txt', depth: 10 }]);
+  },
+
   nodeModulesTransform: {
     type: 'none',
   },
