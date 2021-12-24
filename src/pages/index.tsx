73aeccf7114c45670e8dbc0351ddb26fc18d30bd
Floris Van den Abeele <floris.vandenabeele@external.atlascopco.com>
2021-12-24 09:54:24 +0100

    init CosmosClient
    Leads to following error in dev server:
    TypeError: _url__WEBPACK_IMPORTED_MODULE_0__.URL is not a constructor
    
    checkURL
    ./node_modules/@azure/cosmos/dist-esm/src/utils/checkURL.js:5
    ```
      2 | // Licensed under the MIT license.
      3 | import { URL } from "./url";
      4 | export function checkURL(testString) {
    > 5 |     return new URL(testString);
      6 | }
      7 | export function sanitizeEndpoint(url) {
      8 |     return new URL(url).href.replace(/\/$/, "");
    ```

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,24 @@
 import styles from './index.less';
 
+import { CosmosClient } from '@azure/cosmos';
+const cosmosDbInfo = {
+  endpoint: 'https://mycosmosdb.documents.azure.com',
+  database: 'myDatabase',
+  container: 'myContainer',
+};
+const myResourceToken = 'foobar';
+const resourcePartitionKey = 'myPartionKey';
+
 export default function IndexPage() {
+  const { endpoint, database, container } = cosmosDbInfo;
+
+  const client = new CosmosClient({
+    endpoint,
+    tokenProvider: (request) => Promise.resolve(myResourceToken),
+  })
+    .database(database)
+    .container(container);
+
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
